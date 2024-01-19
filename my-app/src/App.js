68d83985254ad172149888c5b4a754725ce49672
IT Recipe <itrecipe@naver.com>
2024-01-19 15:28:20 +0900

    REACTWORK
    24.01.19 React & SpringBoot 연동 학습

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,40 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
-
-// 0.React = 엔진 -> 데이터변경을 감지하여 UI를 그려주는 엔진
-// 1.실행과정 (index.html) - SPA(Single page application)
-// 2.JSX문법
-// 3.바벨(자바스크립트 ES5) -> ES6
-
-// (1) return시 1개의 DOM만 리턴 가능하다.
-// (2) 변수선언은 let or const로만 해야한다. (JavaScript는 타입이 없음)
-// (3) if 사용 불가 (x) -> 삼항연산자 (O) (조건 ? 값(true) : 값(false))
-// (4) 조건부 렌더링 (조건 && 값(true)) flase는 없음
-// (5) css 디자인
-// - 내부에 적는 방법
-// - 외부 파일에 적는 방법
-// - 라이브러리 사용 (부트스트랩, componet-styled)
-
-let a = 10; // 변수
-const b = 20; // 상수
+import { useState } from 'react';
+import Sub from './Sub';
 
 function App() {
-  let c;
-  //let d = undefined;
+  //let number = 1; //상태 값 아님
+  const [number, setNumber] = useState(2); //React안에 외부 라이브러리 hooks 라이브러리 상태값이 됨.
 
-  console.log(1, c);
-
-  const mystyle = {
-    color: 'red',
+  const add = () => {
+    setNumber(number + 1); //리엑트한테 number 값을 변경한다고 요청
+    console.log('add', number);
   };
 
+  //렌더링 시점 = 상태값 변경
   return (
     <div>
-      <div style={{ mystyle }}>
-        Test {a === 10 ? '10 입니다.' : '10이 아닙니다.'}
-      </div>
-      {<h1 className="box-style">중입니다. {b === 20 && '20 입니다'} </h1>}
-      <hr />
+      <h1>숫자 : {number}</h1>
+      <button onClick={add}>더하기</button>
+      <Sub />
     </div>
   );
 }
---
+++ my-app/src/Sub.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+//새로운 js 파일을
+
+const Sub = () => {
+  return;
+  <div>
+    <h1>Sub입니다.</h1>
+  </div>;
+};
+
+<h1>Sub입니다.</h1>;
+export default Sub;
